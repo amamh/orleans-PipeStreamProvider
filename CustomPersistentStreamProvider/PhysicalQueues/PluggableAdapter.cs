ddf674eab343148e6b5118d10a2cd05d208e1a64
Space Monkey <amamh3@gmail.com>
2015-12-09 17:35:49 +0000

    wtf

--- CustomPersistentStreamProvider/PhysicalQueues/PluggableAdapter.cs
+++ CustomPersistentStreamProvider/PhysicalQueues/PluggableAdapter.cs
@@ -21,15 +21,15 @@ namespace PipeStreamProvider.PhysicalQueues
         private readonly IProviderConfiguration _config;
         private readonly IProviderQueue _queueProvider;
 
-        public GenericQueueAdapter(Logger logger, IStreamQueueMapper streamQueueMapper, string name, IProviderConfiguration config, IProviderQueue queueProvider, int numOfQueues)
+        public GenericQueueAdapter(Logger logger, IStreamQueueMapper streamQueueMapper, string providerName, IProviderConfiguration config, IProviderQueue queueProvider, int numOfQueues)
         {
-            Name = name;
+            Name = providerName; // WTF: If you set the name to anything else, the client won't receive any messages !?????
             _config = config;
             _logger = logger;
             _streamQueueMapper = streamQueueMapper;
             _queueProvider = queueProvider;
 
-            queueProvider.Init(_logger, _config, name, numOfQueues);
+            queueProvider.Init(_logger, _config, providerName, numOfQueues);
         }
 
         public Task QueueMessageBatchAsync<T>(Guid streamGuid, string streamNamespace, IEnumerable<T> events, StreamSequenceToken token,
@@ -60,7 +60,7 @@ namespace PipeStreamProvider.PhysicalQueues
             return new GenericQueueAdapterReceiver(_logger, queueId, _queueProvider);
         }
 
-        public string Name { get; private set; }
+        public string Name { get; }
         public bool IsRewindable { get; } = true;
 
         public StreamProviderDirection Direction => StreamProviderDirection.ReadWrite;
