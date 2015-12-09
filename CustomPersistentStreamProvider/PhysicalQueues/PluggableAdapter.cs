10995cdde84b169b1d4d49dcfb87f789deb41558
Space Monkey <amamh3@gmail.com>
2015-12-09 15:22:10 +0000

    init

--- CustomPersistentStreamProvider/PhysicalQueues/PluggableAdapter.cs
+++ CustomPersistentStreamProvider/PhysicalQueues/PluggableAdapter.cs
@@ -10,10 +10,11 @@ using Orleans.Runtime;
 using Orleans.Serialization;
 using Orleans.Streams;
 using StackExchange.Redis;
+using Orleans.Providers;
 
 namespace PipeStreamProvider.PhysicalQueues.Redis
 {
-    public class RedisQueueAdapter : IQueueAdapter
+    public class GenericQueueAdapter : IQueueAdapter
     {
         private Logger _logger;
         private IStreamQueueMapper _streamQueueMapper;
@@ -22,32 +23,18 @@ namespace PipeStreamProvider.PhysicalQueues.Redis
         //private readonly string _server;
         private IDatabase _database;
         private string _redisListBaseName = "OrleansQueue";
+        private readonly IProviderConfiguration _config;
+        private readonly IProviderQueue _queueProvider;
 
-        public RedisQueueAdapter(Logger logger, IStreamQueueMapper streamQueueMapper, string name, string server, int database)
+        public GenericQueueAdapter(Logger logger, IStreamQueueMapper streamQueueMapper, string name, IProviderConfiguration config, IProviderQueue queueProvider)
         {
-            // I'm responsible for the connection
-            _connection = ConnectionMultiplexer.ConnectAsync(server).Result;
-            _database = _connection.GetDatabase(database);
-            logger.AutoInfo($"connection to Redis successful.");
-
-            Init(logger, streamQueueMapper, name, _database);
-        }
-
-        public RedisQueueAdapter(Logger logger, IStreamQueueMapper streamQueueMapper, string name, IDatabase db)
-        {
-            Init(logger, streamQueueMapper, name, db);
-        }
-
-        void Init(Logger logger, IStreamQueueMapper streamQueueMapper, string name, IDatabase db)
-        {
+            Name = name;
+            _config = config;
             _logger = logger;
             _streamQueueMapper = streamQueueMapper;
+            _queueProvider = queueProvider;
 
-            _database = db;
-
-            Name = name;
-
-            _redisListBaseName = $"{Environment.MachineName}-{DateTime.UtcNow}-orleans-{Name}-queue";
+            queueProvider.Init(_config);
         }
 
         public Task QueueMessageBatchAsync<T>(Guid streamGuid, string streamNamespace, IEnumerable<T> events, StreamSequenceToken token,
@@ -60,7 +47,6 @@ namespace PipeStreamProvider.PhysicalQueues.Redis
             }
 
             var queueId = _streamQueueMapper.GetQueueForStream(streamGuid, streamNamespace);
-            var redisListName = GetRedisListName(queueId);
 
             var eventsAsObjects = events.Cast<object>().ToList();
 
@@ -68,32 +54,15 @@ namespace PipeStreamProvider.PhysicalQueues.Redis
 
             var bytes = SerializationManager.SerializeToByteArray(container);
 
-            try
-            {
-                if (_database == null)
-                {
-                    _logger.AutoWarn($"Trying to write before connection is made to Redis. This batch of data was ignored.");
-                    return TaskDone.Done;
-                }
-
-                _database.ListLeftPush(redisListName, bytes);
-            }
-            catch (Exception exception)
-            {
-                _logger.AutoError($"failed to write to Redis list {redisListName}. Exception: {exception}");
-            }
+            _queueProvider.Enqueue(queueId, bytes);
 
             return TaskDone.Done;
         }
 
-        private string GetRedisListName(QueueId queueId)
-        {
-            return $"{_redisListBaseName}-{queueId}";
-        }
 
         public IQueueAdapterReceiver CreateReceiver(QueueId queueId)
         {
-            return new RedisQueueAdapterReceiver(_logger, queueId, _database, GetRedisListName(queueId));
+            return new GenericQueueAdapterReceiver(_logger, queueId, _database, queueId, _queueProvider);
         }
 
         public string Name { get; private set; }
---
+++ CustomPersistentStreamProvider/PhysicalQueues/PluggableReceiver.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Orleans;
+using Orleans.Providers.Streams.Common;
+using Orleans.Runtime;
+using Orleans.Serialization;
+using Orleans.Streams;
+
+namespace PipeStreamProvider.PhysicalQueues.Redis
+{
+    public class GenericQueueAdapterReceiver : IQueueAdapterReceiver
+    {
+        private readonly Logger _logger;
+        public QueueId Id { get; }
+        private long _sequenceId;
+        private readonly IProviderQueue _queueProvider;
+
+        public GenericQueueAdapterReceiver(Logger logger, QueueId queueid, IProviderQueue queueProvider)
+        {
+            _queueProvider = queueProvider;
+            _logger = logger;
+
+            Id = queueid;
+        }
+
+        public Task Initialize(TimeSpan timeout)
+        {
+            return TaskDone.Done;
+        }
+
+        public Task<IList<IBatchContainer>> GetQueueMessagesAsync(int maxCount)
+        {
+            var listOfMessages = new List<byte[]>();
+
+            var listLength = _queueProvider.Length(Id);
+            var max = Math.Max(maxCount, listLength);
+
+            for (var i = 0; i < max; i++)
+            {
+                var nextMsg = _queueProvider.Dequeue(Id);
+                listOfMessages.Add(nextMsg);
+            }
+
+            var list = (from m in listOfMessages select SerializationManager.DeserializeFromByteArray<PipeQueueAdapterBatchContainer>(m));
+            var pipeQueueAdapterBatchContainers = list as IList<PipeQueueAdapterBatchContainer> ?? list.ToList();
+            foreach (var batchContainer in pipeQueueAdapterBatchContainers)
+                batchContainer.SimpleSequenceToken = new SimpleSequenceToken(_sequenceId++);
+
+            _logger.AutoVerbose($"Read {pipeQueueAdapterBatchContainers.Count} batch containers");
+            return Task.FromResult<IList<IBatchContainer>>(pipeQueueAdapterBatchContainers.ToList<IBatchContainer>());
+        }
+
+        public Task MessagesDeliveredAsync(IList<IBatchContainer> messages)
+        {
+            var count = messages == null ? 0 : messages.Count;
+            var lastToken = messages?.Count != 0 ? messages?.Last()?.SequenceToken.ToString() : "--";
+            _logger.AutoVerbose($"Delivered {count}, last one has token {lastToken}");
+            return TaskDone.Done;
+        }
+
+        public Task Shutdown(TimeSpan timeout)
+        {
+            _logger.AutoInfo("Receiver requested to shutdown.");
+            return TaskDone.Done;
+        }
+    }
+}
